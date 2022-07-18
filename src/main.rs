48e799ef57ae3d03da55eedbe241d4f3e37684c4
Brendan Dagys <brendandagys@gmail.com>
2022-07-17 20:39:42 -0400

    Implement parallelization with threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use chrono::{DateTime, Utc};
-use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use reqwest::{
+    blocking::Client,
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+};
+use serde::Deserialize;
 use serde_json::{json, Value};
-use std::{fs, ops::Range};
+use std::{
+    fs,
+    ops::Range,
+    thread::{self, JoinHandle},
+};
 
 fn print_time(prefix: &str) -> DateTime<Utc> {
     let now = Utc::now();
@@ -10,45 +18,71 @@ fn print_time(prefix: &str) -> DateTime<Utc> {
 }
 
 async fn test_rust() {
-    let start_time = print_time("RUST START");
+    let start_time = print_time("\nRUST START");
 
     let path = "./src/test_data.json";
     let data = fs::read_to_string(path).expect("Unable to read file!");
-    let mut json_payload: Value = serde_json::from_str(&data).expect("Failed to parse JSON!");
 
-    let client = reqwest::Client::new();
+    let mut handles: Vec<JoinHandle<()>> = vec![];
 
-    for num in (Range { start: 0, end: 20 }) {
-        json_payload["invoice_number"] = json!(num.to_string());
+    for num in (Range::<i32> { start: 0, end: 20 }) {
+        let mut json_payload: Value = serde_json::from_str(&data).expect("Failed to parse JSON!");
 
-        let response = client
-            .post("http://0.0.0.0:8001/invoice")
-            .header(CONTENT_TYPE, "application/json")
-            .header(ACCEPT, "application/json")
-            .json(&json_payload)
-            .send()
-            .await
-            .unwrap();
+        let join_handle = thread::spawn(move || {
+            json_payload["invoice_number"] = json!(num.to_string());
 
-        println!(
-            "{num} Response: {:?}",
-            response.headers().get("date").unwrap()
-        );
+            let client = Client::new();
+
+            let response = client
+                .post("http://0.0.0.0:8001/invoice")
+                .header(CONTENT_TYPE, "application/json")
+                .header(ACCEPT, "application/json")
+                .json(&json_payload)
+                .send()
+                .expect("Request failed!");
+
+            println!(
+                "Thread {:0>2} response: {:?}",
+                num,
+                response.text().unwrap() // response.headers().get("date").unwrap()
+            )
+        });
+
+        handles.push(join_handle)
+    }
+
+    for handle in handles {
+        handle.join().unwrap()
     }
 
     let end_time = print_time("RUST END");
     println!(
-        "\nTime taken: {} seconds",
+        "\nRUST TIME TAKEN: {} seconds",
         (end_time - start_time).num_milliseconds() as f64 / 1000.0
     )
 }
 
 async fn test_node() {
-    let start_time = print_time("NODE START");
+    #[derive(Deserialize)]
+    struct Payload {
+        success: bool,
+        invoicePdfs: Vec<String>,
+        timeTaken: f64,
+    }
+
+    #[derive(Deserialize)]
+    struct Data {
+        actionResponse: Payload,
+    }
+
+    #[derive(Deserialize)]
+    struct NodeResponse {
+        data: Data,
+    }
 
     let client = reqwest::Client::new();
 
-    client
+    let response = client
         .post("http://0.0.0.0:5000/api/actions")
         .header(AUTHORIZATION, "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQyMjIsImF1dGhTdHJpbmciOiJhO2tsc2RqZjtrcGx3ZXFycml1cG90MnF3LXVwOWlvdHJmamlwW293cWVuO21rZGxmZ3ZhO2xpa2pwaHUgc2siLCJpYXQiOjE2MTU4NDM1MjB9.35QdvO_SGp5femVaTK-S9jsIikPD8WSc4JObxME-c5E")
         .json(&json!({ "action": "testPdfGeneration" }))
@@ -56,18 +90,16 @@ async fn test_node() {
         .await
         .unwrap();
 
-    let end_time = print_time("NODE END");
+    let response_json = response.json::<NodeResponse>().await.unwrap();
+
     println!(
-        "\nTime taken: {} seconds",
-        (end_time - start_time).num_milliseconds() as f64 / 1000.0
+        "NODE TIME TAKEN: {} seconds",
+        response_json.data.actionResponse.timeTaken
     )
 }
 
 #[tokio::main]
 async fn main() {
     test_rust().await;
-
-    println!("\n===================================\n");
-
     test_node().await;
 }
